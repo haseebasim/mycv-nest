62a8c8a68f37551c2868b46ecc7b196b2f94991f
Syed Haseeb Asim Zaidi <45163316+haseebasim@users.noreply.github.com>
2023-04-15 18:18:44 +0500

    added serialize interceptor for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,10 +7,12 @@ import {
   Patch,
   Post,
   Query,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/creat-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 
 @Controller('auth')
 export class UsersController {
@@ -18,22 +20,23 @@ export class UsersController {
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    this.usersService.create(body.email, body.password);
+    return this.usersService.create(body.email, body.password);
   }
 
+  @UseInterceptors(SerializeInterceptor)
   @Get('/:id')
   findUser(@Param('id') id: string) {
-    this.usersService.findOne(parseInt(id));
+    return this.usersService.findOne(parseInt(id));
   }
 
   @Get()
   findAllUser(@Query('email') email: string) {
-    this.usersService.find(email);
+    return this.usersService.find(email);
   }
 
   @Delete('/:id')
   removeUser(@Param('id') id: string) {
-    this.usersService.findOne(parseInt(id));
+    return this.usersService.findOne(parseInt(id));
   }
 
   @Patch('/:id')
